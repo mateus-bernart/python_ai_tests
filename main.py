5c586b289bc2c94cd3c973488dc725711b03958b
Mateus Bernart <mateusbernart14@gmail.com>
2026-03-19 21:19:32 -0300

    adds formatting objects to langchain and chain of processes

--- main.py
+++ main.py
@@ -1,33 +1,64 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate 
+from  langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+from langchain_core.globals import set_debug
 import os
+
+set_debug(True)
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
-numero_dias = 7
-numero_criancas = 2
-atividade = "musica"
+class Destino(BaseModel):
+  cidade: str = Field("A cidade recomendada para visitar")
+  motivo: str = Field("motivo pelo qual é interessante visitar essa cidade")
 
-modelo_de_prompt = PromptTemplate(
+class Restaurantes(BaseModel):
+  cidade: str = Field("A cidade recomendada para visitar")
+  restaurantes: str = Field("Restaurantes recomendados na cidade")
+ 
+parseador_destino = JsonOutputParser(pydantic_object=Destino)
+parseador_restaurantes = JsonOutputParser(pydantic_object=Restaurantes)
+
+prompt_cidade = PromptTemplate( 
   template="""
-  Crie um roteiro de viagens para um periodo de {dias} para uma familia com {numero_criancas} criancas e a atividade {atividade}
-  """
+  Sugira uma cidade dado ao meu intere por {interesse}.
+  {formato_de_saida}
+  """,
+  input_variables=["interesse"], 
+  partial_variables={"formato_de_saida": parseador_destino.get_format_instructions()}
 )
 
-prompt = modelo_de_prompt.format(
-  dias=numero_dias, 
-  numero_criancas=numero_criancas,
-  atividade=atividade
+prompt_restaurantes = PromptTemplate( 
+  template="""
+  Sugira restaurantes populares entre locais em {cidade}.
+  {formato_de_saida}
+  """,
+  partial_variables={"formato_de_saida": parseador_restaurantes.get_format_instructions()}
 )
 
-modelo = ChatOpenAI(
+prompt_cultural = PromptTemplate( 
+  template="Sugira atrações culturais na cidade de {cidade}.",
+)
+
+modelo = ChatOpenAI( 
   model="gpt-3.5-turbo",
   temperature=0.5,
   api_key=api_key
 )
 
-resposta = modelo.invoke(prompt)
+cadeia_1 = prompt_cidade | modelo | parseador_destino
+cadeia_2 = prompt_restaurantes | modelo | parseador_restaurantes
+cadeia_3 = prompt_cultural | modelo | StrOutputParser()
+
+cadeia = (cadeia_1 | cadeia_2 | cadeia_3)
+
+resposta = cadeia.invoke(
+  {
+    "interesse": "praias"
+  }
+)
 
 print(resposta)
